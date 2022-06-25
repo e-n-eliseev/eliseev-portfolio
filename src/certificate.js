824b0bac1f2393077af7354f69dc498d74fe574b
e-n-eliseev <e.n.eliseev@mail.ru>
2022-06-25 13:57:46 +0300

    descriptions of certificates

--- src/certificate.js
+++ src/certificate.js
@@ -1,12 +1,12 @@
 
 export const certificate = [
     ["Профессиональная вёрстка", "GeekBrains", "HTML5.CSS/SAAS.BootStrap. Позиционирование элементов, семантика, работа с формами, препроцессорами, анимацией, адаптивная верстка.", "https://gb.ru/certificates/1424672?76a185a653dacab278d8439a4fb793b6"],
-    ["Профессиональная вёрстка", "GeekBrains", "Сложный уровень тестирования.", "https://gb.ru/go/OjMJnb"],
+    ["Профессиональная вёрстка", "GeekBrains", "cложный уровень тестирования.", "https://gb.ru/go/OjMJnb"],
     ["Продвинутый курс JavaScript", "GeekBrains", "ES6+, работа с асинхронными запросами, ООП, работа с сервером.", "https://gb.ru/go/e9Uk1A"],
-    ["Продвинутый курс JavaScript", "GeekBrains", "Сложный уровень тестирования.", "https://gb.ru/go/BcUmFp"],
-    ["Сборка Web-проектов", "GeekBrains", "WebPack.", "https://gb.ru/go/rwPiyd"],
+    ["Продвинутый курс JavaScript", "GeekBrains", "cложный уровень тестирования.", "https://gb.ru/go/BcUmFp"],
+    ["Сборка Web-проектов", "GeekBrains", "работа со сборщиком WebPack.", "https://gb.ru/go/rwPiyd"],
     ["Vue", "GeekBrains", "Vue и его главные концепции. Работа с директивами и данными, роутингом, VueX. Оптимизация и тестирование приложений.", "https://gb.ru/go/6Vd__E"],
     ["React", "GeekBrains", "React и его концепции. Жизненый цикл, роутинг, Redux. Работа с API, FireBase. Оптимизация и тестирование приложений.", "https://gb.ru/go/1w2BVI"],
-    ["NodeJs", "GeekBrains", "Работа с файловой системой, цикл событий, создание и настройка сервера, Socket.io", "https://gb.ru/go/E9l1Ou"],
+    ["NodeJs", "GeekBrains", "работа с файловой системой, цикл событий, создание и настройка сервера, Socket.io", "https://gb.ru/go/E9l1Ou"],
     ["Основы реляционных баз данных. MySQL", "GeekBrains", "CRUD операции, агрегация данных, составные запросы, транзакции", "https://gb.ru/go/6yxHdV"],
 ]
